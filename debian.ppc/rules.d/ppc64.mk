fb025ff620f2a0179bb7a7e2ea100338e3787767
Tim Gardner <tim.gardner@canonical.com>
2012-11-07 07:59:37 -0700

    UBUNTU: Created debian.ppc
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

---
+++ debian.ppc/rules.d/ppc64.mk
@@ -0,0 +1,23 @@
+human_arch	= PowerPC (64 bit userspace)
+build_arch	= powerpc
+header_arch	= $(build_arch)
+defconfig	= ppc64_defconfig
+flavours	= powerpc64-smp
+build_image	= vmlinux
+kernel_file	= $(build_image)
+install_file	= $(build_image)
+
+loader		= yaboot
+
+custom_flavours	=
+
+no_dumpfile = true
+skipdbg		= true
+skipabi		= true
+skipmodule	= true
+do_doc_package          = false
+do_source_package       = false
+do_common_headers_indep = false
+do_libc_dev_package     = false
+
+family=ubuntu
