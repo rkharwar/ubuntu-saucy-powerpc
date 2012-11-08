c51a6fa5986b548499e522439494f29dc985c2fd
Ben Collins <ben.c@servergy.com>
2012-11-08 15:03:50 -0500

    UBUNTU: [Config] Update features enabled on PowerPC build
    Signed-off-by: Ben Collins <ben.c@servergy.com>

--- debian.ppc/rules.d/powerpc.mk
+++ debian.ppc/rules.d/powerpc.mk
@@ -7,18 +7,25 @@ build_image	= vmlinux
 kernel_file	= $(build_image)
 install_file	= $(build_image)
 
+# These flavours differ
+build_image_powerpc-e500mc	= uImage
+kernel_file_powerpc-e500mc	= arch/powerpc/boot/uImage
+
+build_image_powerpc-e500	= uImage
+kernel_file_powerpc-e500	= arch/powerpc/boot/uImage
+
 loader		= yaboot
 
 custom_flavours	=
 
 no_dumpfile		= true
 skipdbg			= true
-skipabi			= false
-skipmodule		= false
+skipabi			= true
+skipmodule		= true
 do_doc_package          = false
 do_source_package       = false
 do_common_headers_indep = true
-do_libc_dev_package     = true
+do_libc_dev_package     = false
 do_tools		= false
 
 family			= ubuntu
