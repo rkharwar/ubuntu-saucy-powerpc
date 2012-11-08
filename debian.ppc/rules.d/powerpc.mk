514e3d50b7e47cd32ecd0ef52f8f1d33d29ead48
Ben Collins <ben.c@servergy.com>
2012-11-07 22:14:11 -0500

    UBUNTU: [Config] Final configs and rules for PowerPC
    Signed-off-by: Ben Collins <ben.c@servergy.com>

--- debian.ppc/rules.d/powerpc.mk
+++ debian.ppc/rules.d/powerpc.mk
@@ -2,7 +2,7 @@ human_arch	= PowerPC (32 bit userspace)
 build_arch	= powerpc
 header_arch	= $(build_arch)
 defconfig	= pmac32_defconfig
-flavours	= powerpc-smp powerpc64-smp
+flavours	= powerpc-smp powerpc64-smp powerpc-e500 powerpc-e500mc
 build_image	= vmlinux
 kernel_file	= $(build_image)
 install_file	= $(build_image)
@@ -11,13 +11,13 @@ loader		= yaboot
 
 custom_flavours	=
 
-no_dumpfile = true
-skipdbg		= true
-skipabi		= true
-skipmodule	= true
+no_dumpfile		= true
+skipdbg			= true
+skipabi			= false
+skipmodule		= false
 do_doc_package          = false
 do_source_package       = false
-do_common_headers_indep = false
-do_libc_dev_package     = false
+do_common_headers_indep = true
+do_libc_dev_package     = true
 
-family=ubuntu
+family			= ubuntu
