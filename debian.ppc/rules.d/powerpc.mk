4df316475d8f7c77e7a696745a30fd5f16b36883
Ben Collins <ben.c@servergy.com>
2013-07-10 16:34:34 -0400

    UBUNTU: PPC-Ubuntu-3.10.0-0.0
    Ignore: yes
    Signed-off-by: Ben Collins <ben.c@servergy.com>

---
+++ debian.ppc/rules.d/powerpc.mk
@@ -0,0 +1,31 @@
+human_arch	= PowerPC (32 bit userspace)
+build_arch	= powerpc
+header_arch	= $(build_arch)
+defconfig	= pmac32_defconfig
+flavours	= powerpc-smp powerpc64-smp powerpc-e500 powerpc-e500mc
+build_image	= vmlinux
+kernel_file	= $(build_image)
+install_file	= $(build_image)
+
+# These flavours differ
+build_image_powerpc-e500mc	= uImage
+kernel_file_powerpc-e500mc	= arch/powerpc/boot/uImage
+
+build_image_powerpc-e500	= uImage
+kernel_file_powerpc-e500	= arch/powerpc/boot/uImage
+
+loader		= yaboot
+
+custom_flavours	=
+
+skipabi = false
+skipmodules = false
+
+no_dumpfile		= true
+do_doc_package          = false
+do_source_package       = true
+do_common_headers_indep = false
+do_libc_dev_package     = false
+do_tools		= false
+
+family			= ubuntu
